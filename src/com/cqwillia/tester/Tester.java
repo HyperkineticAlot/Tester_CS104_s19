067293fe8f37d432fe0b5a7da197b8ca9246fea6
Cameron Williams <cqwillia@usc.edu>
2019-01-25 18:08:22 -0800

    completed addition of (non-functional) UI elements to preferences Panel

--- src/com/cqwillia/tester/Tester.java
+++ src/com/cqwillia/tester/Tester.java
@@ -17,12 +17,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Flow;
 
 public class Tester
 {
     private TesterInterface gui;
     private File testDirectory;
     private ArrayList<String> preferences;
+    private ArrayList<String> defaultPrefs;
 
     private BufferedReader reader;
     private PrintStream console;
@@ -58,11 +60,24 @@ public class Tester
 
     private void init()
     {
-        //read from preferences file into preferences arraylist
-        preferences = new ArrayList<> (7);
+        //read from preferences file into preferences arraylist and default preferences arraylist
+        defaultPrefs = new ArrayList<>(7);
+        for(int i = 0; i < 7; i++) {
+            defaultPrefs.add("");
+        }
+
+        preferences = (ArrayList<String>)defaultPrefs.clone();
 
         //initialise the gui
-        gui = new TesterInterface();
+        try
+        {
+            EventQueue.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    gui = new TesterInterface();
+                }
+            });
+        } catch(Exception e) { e.printStackTrace(); }
         gui.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -98,8 +113,8 @@ public class Tester
                     }
                 } catch(IOException e)
                 {
-                    System.out.println("Input redirection to console threw an IO exception");
-                    e.printStackTrace();
+                    /*System.out.println("Input redirection to console threw an IO exception");
+                    e.printStackTrace();*/
                 }
             }
         };
@@ -123,12 +138,13 @@ public class Tester
     }
 
 
-    public void updateField(Field f, String s)
+    private void updateField(Field f, String s)
     {
         File file = new File(s);
         switch(f)
         {
             case WORKING_DIRECTORY:
+                if (preferences.get(I_WDIR).equals(s)) return;
                 if(Files.exists(file.toPath()))
                 {
                     try {
@@ -154,6 +170,7 @@ public class Tester
                 preferences.set(I_TESTNAME, s);
 
             case INPUT_DIR:
+                if (preferences.get(I_INDIR).equals(s)) return;
                 if(Files.exists(file.toPath()))
                 {
                     try {
@@ -184,6 +201,7 @@ public class Tester
                 }
 
             case OUTPUT_DIR:
+                if (preferences.get(I_OUTDIR).equals(s)) return;
                 if(Files.exists(file.toPath()))
                 {
                     try {
@@ -226,6 +244,7 @@ public class Tester
                 }
 
             case REFERENCE_DIR:
+                if (preferences.get(I_REFDIR).equals(s)) return;
                 if(Files.exists(file.toPath()))
                 {
                     try {
@@ -256,6 +275,7 @@ public class Tester
                 }
 
             case TEST_PATH:
+                if (preferences.get(I_TESTPATH).equals(s)) return;
                 if(Files.exists(file.toPath()))
                 {
                     try {
@@ -285,6 +305,51 @@ public class Tester
                     }
                 }
         }
+    }
+
+    private void restoreAllFields()
+    {
+        gui.restore(Field.WORKING_DIRECTORY, preferences.get(I_WDIR));
+        gui.restore(Field.INPUT_DIR, preferences.get(I_INDIR));
+        gui.restore(Field.OUTPUT_DIR, preferences.get(I_OUTDIR));
+        gui.restore(Field.REFERENCE_DIR, preferences.get(I_REFDIR));
+        gui.restore(Field.HOMEWORK_NUM, preferences.get(I_HWNUM));
+        gui.restore(Field.TEST_NAME, preferences.get(I_TESTNAME));
+        gui.restore(Field.TEST_PATH, preferences.get(I_TESTPATH));
+    }
+
+    private void restoreAllDefaults()
+    {
+        gui.restore(Field.WORKING_DIRECTORY, defaultPrefs.get(I_WDIR));
+        gui.restore(Field.INPUT_DIR, defaultPrefs.get(I_INDIR));
+        gui.restore(Field.OUTPUT_DIR, defaultPrefs.get(I_OUTDIR));
+        gui.restore(Field.REFERENCE_DIR, defaultPrefs.get(I_REFDIR));
+        gui.restore(Field.HOMEWORK_NUM, defaultPrefs.get(I_HWNUM));
+        gui.restore(Field.TEST_NAME, defaultPrefs.get(I_TESTNAME));
+        gui.restore(Field.TEST_PATH, defaultPrefs.get(I_TESTPATH));
+    }
+
+    private String getField(Field f)
+    {
+        switch(f)
+        {
+            case WORKING_DIRECTORY:
+                return preferences.get(I_WDIR);
+            case HOMEWORK_NUM:
+                return preferences.get(I_HWNUM);
+            case TEST_NAME:
+                return preferences.get(I_TESTNAME);
+            case INPUT_DIR:
+                return preferences.get(I_INDIR);
+            case OUTPUT_DIR:
+                return preferences.get(I_OUTDIR);
+            case REFERENCE_DIR:
+                return preferences.get(I_REFDIR);
+            case TEST_PATH:
+                return preferences.get(I_TESTPATH);
+        }
+
+        return null;
     }
 
     private void runCommands(String comm, File inDir, File outDir)
@@ -326,21 +391,24 @@ public class Tester
         private JTextField refDir;
         private JTextField testPath;
 
+        private JComboBox<String> hwNum;
+        private JComboBox<String> testName;
+
         protected TesterInterface()
         {
             //set default values for tester window
             super("CS 104 Spring 2018 Test Case Manager v0.0.2");
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setSize(800, 600);
+            setMinimumSize(new Dimension(800, 600));
             setResizable(false);
             setLayout(new GridLayout(3, 1));
 
-            /*//initialise console text area and add to pane
+            //initialise console text area and add to pane
             consoleWindow = new JTextArea();
             consoleWindow.setEditable(false);
             getContentPane().add(consoleWindow);
 
-            //construct the menuBar and menu items, then add them to the frame
+            /*//construct the menuBar and menu items, then add them to the frame
             JMenuBar menuBar = new JMenuBar();
             JMenu fileMenu = new JMenu("File");
             fileMenu.setMnemonic(KeyEvent.VK_F);
@@ -363,10 +431,90 @@ public class Tester
             prefPanel.setBorder(BorderFactory.createCompoundBorder(border, bevel));
             GridBagLayout prefLayout = new GridBagLayout();
             prefLayout.columnWidths = new int[] { 370, 370 };
-            prefLayout.rowHeights = new int[] { 50, 50, 30, 30, 30, 70 };
+            //prefLayout.rowHeights = new int[] { 70, 100, 30, 30, 70 };
+            prefPanel.setLayout(prefLayout);
 
+            //initialise working directory input and mounting panel
+            JPanel workPanel = new JPanel();
+            workPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+            workDir = new JTextField(getField(Field.WORKING_DIRECTORY), 55);
+            workPanel.add(new JLabel("Working directory:"));
+            workPanel.add(workDir);
+            GridBagConstraints workConstraints = new GridBagConstraints();
+            setConstraints(workConstraints, 0, 0);
+            workConstraints.gridwidth = 2;
+            prefPanel.add(workPanel, workConstraints);
 
-            add(prefPanel);
+            //initialise dropdown menus for homework and test name, and pin to mounting panels
+            JPanel hwPanel = new JPanel();
+            hwPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
+            hwNum = new JComboBox<>( new String[]{ "Homework 2" } );
+            hwPanel.add(new JLabel("Homework number:"));
+            hwPanel.add(hwNum);
+            GridBagConstraints hwConstraints = new GridBagConstraints();
+            setConstraints(hwConstraints, 0, 1);
+            prefPanel.add(hwPanel, hwConstraints);
+
+            JPanel testPanel = new JPanel();
+            testName = new JComboBox<>( new String[]{"split", "ulliststr"});
+            testPanel.add(new JLabel("Test name:"));
+            testPanel.add(testName);
+            GridBagConstraints testConstraints = new GridBagConstraints();
+            setConstraints(testConstraints, 1, 1);
+            prefPanel.add(testPanel, testConstraints);
+
+            //initialise "directory panels" for paths to input, output, reference directories, and test script path
+            inDir = new JTextField(getField(Field.INPUT_DIR), 15);
+            makeDirectoryPanel(prefPanel, inDir, "Input directory:", 0, 2);
+
+            outDir = new JTextField(getField(Field.OUTPUT_DIR), 15);
+            makeDirectoryPanel(prefPanel, outDir, "Output directory:", 1, 2);
+
+            refDir = new JTextField(getField(Field.REFERENCE_DIR), 15);
+            makeDirectoryPanel(prefPanel, refDir, "Reference directory:", 0, 3);
+
+            testPath = new JTextField(getField(Field.TEST_PATH), 15);
+            makeDirectoryPanel(prefPanel, testPath, "Test script path:", 1, 3);
+
+            //initialise buttons and button panel
+            JPanel buttonPanel = new JPanel();
+            JButton restoreButton = new JButton("Restore defaults");
+            restoreButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    restoreAllDefaults();
+                }
+            });
+            JButton cancelButton = new JButton("Cancel");
+            cancelButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    restoreAllFields();
+                }
+            });
+            JButton saveButton = new JButton("Save");
+            saveButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    updateField(Field.WORKING_DIRECTORY, workDir.getText());
+                    updateField(Field.INPUT_DIR, inDir.getText());
+                    updateField(Field.OUTPUT_DIR, outDir.getText());
+                    updateField(Field.REFERENCE_DIR, refDir.getText());
+                    updateField(Field.TEST_PATH, testPath.getText());
+
+                    updateField(Field.HOMEWORK_NUM, (String) hwNum.getSelectedItem());
+                    updateField(Field.TEST_NAME, (String) testName.getSelectedItem());
+                }
+            });
+            buttonPanel.add(restoreButton);
+            buttonPanel.add(cancelButton);
+            buttonPanel.add(saveButton);
+            GridBagConstraints buttonConstraints = new GridBagConstraints();
+            buttonConstraints.gridwidth = 2;
+            setConstraints(buttonConstraints, 0, 4);
+            prefPanel.add(buttonPanel, buttonConstraints);
+
+            getContentPane().add(prefPanel);
 
             setVisible(true);
         }
@@ -386,6 +534,26 @@ public class Tester
                 case TEST_PATH:
                     testPath.setText(s);
             }
+        }
+
+        private void setConstraints(GridBagConstraints c, int x, int y)
+        {
+            c.gridx = x;
+            c.gridy = y;
+            c.weighty = 1;
+            c.weightx = 1;
+            c.fill = GridBagConstraints.BOTH;
+        }
+
+        private void makeDirectoryPanel(JComponent parent, JTextField dirField, String label, int x, int y)
+        {
+            JPanel panel = new JPanel();
+            panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+            panel.add(new JLabel(label));
+            panel.add(dirField);
+            GridBagConstraints c = new GridBagConstraints();
+            setConstraints(c, x, y);
+            parent.add(panel, c);
         }
     }
 }
